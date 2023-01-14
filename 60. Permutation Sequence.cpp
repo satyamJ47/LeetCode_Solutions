5665311aea4d176fa2f3ba462cfb7a2276ae320d
Satyajit Jagadale <satyamjagadale47@gmail.com>
2023-01-14 21:20:13 +0530

    Link: https://leetcode.com/problems/permutation-sequence/

---
+++ 60. Permutation Sequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        int fact=1;
+        vector<int>nums;
+        for(int i=1;i<n;i++)
+        {
+            fact*=i;
+            nums.push_back(i);
+        }
+        nums.push_back(n);
+        k--;
+        string s="";
+        while(1)
+        {
+            s+=to_string(nums[k/fact]);
+            cout<<nums[k/fact]<<endl;
+            nums.erase(nums.begin()+(k/fact));
+            if(!nums.size())break;
+            k%=fact;
+            fact/=nums.size();
+        }
+        return s;
+    }
+};
