1df9da7558d44849a0daa8c6bfb20b6c174d8b51
Satyajit Jagadale <113798693+satyamJ47@users.noreply.github.com>
2023-04-27 23:49:40 +0530

    Link: https://leetcode.com/problems/bulb-switcher/description/

---
+++ 319. Bulb Switcher.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        int cnt = 0;
+        for(int i=1;i<=n;i++)
+        {
+            if(i*i<=n)cnt++;
+            else break;
+        }
+        return cnt;
+    }
+};
