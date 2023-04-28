34dbafd9ce42848a54748abe9d06e69b768ad09d
Satyajit Jagadale <113798693+satyamJ47@users.noreply.github.com>
2023-04-28 21:56:37 +0530

    Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/

---
+++ 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int solve(int i,int buy,vector<int>&prices,int fee,vector<vector<int>>&dp)
+    {
+        if(i == prices.size())return 0;
+        if(dp[i][buy]!=-1)return dp[i][buy];
+        int profit = 0;
+        if(buy) profit = max(-prices[i]+solve(i+1,0,prices,fee,dp),solve(i+1,1,prices,fee,dp));
+        else profit = max(-fee+prices[i]+solve(i+1,1,prices,fee,dp),solve(i+1,0,prices,fee,dp));
+        return dp[i][buy] = profit;
+    }
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size(); 
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return solve(0,1,prices,fee,dp);
+    }
+};
