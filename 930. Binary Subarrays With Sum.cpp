6e659b4da618f8f9c76f91d210db5b19a85a6f4c
Satyajit Jagadale <113798693+satyamJ47@users.noreply.github.com>
2023-08-02 23:48:29 +0530

    Link: https://leetcode.com/problems/binary-subarrays-with-sum/https://leetcode.com/problems/binary-subarrays-with-sum/

---
+++ 930. Binary Subarrays With Sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int sum = 0;
+        unordered_map<int,int>pre_freq;
+        pre_freq[0] = 1;
+        int cnt = 0;
+        for(auto i:nums)
+        {
+            sum+=i;
+            if(pre_freq.find(sum-goal)!=pre_freq.end())cnt+=pre_freq[sum-goal];
+            pre_freq[sum]++;
+        }
+        return cnt;
+    }
+};
